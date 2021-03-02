3ec6cb2d844b3a7627a14b296d1057d4aa2deae4
javierdeporte <javierdeporte@yahoo.com>
2021-03-02 10:56:23 -0500

    codigo con la fecha

--- archivo de texto.R
+++ archivo de texto.R
@@ -1,3 +1,6 @@
 print("Este archivo es de Rstudio")
 print(" Y ahora esta en github")
 print("Primera modificacion")
+
+as.numeric(Sys.time())
+as.POSIXct(21779111.828, origin="1970-01-01", tz="GMT")
